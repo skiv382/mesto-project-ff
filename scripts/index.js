67404449394a73d1fdffb053fee0ebc06f427141
Pashkov Artem <skiv382@gmail.com>
2025-04-23 18:33:15 +0300

    webpack

--- scripts/index.js
+++ scripts/index.js
@@ -32,3 +32,18 @@ initialCards.forEach(cardData => {
   cardsContainer.append(cardElement);
 });
 
+// теперь картинки можно импортировать,
+// вебпак добавит в переменные правильные пути
+const jordanImage = new URL('./images/jordan.jpg', import.meta.url);
+const jamesImage = new URL('./images/james.jpg', import.meta.url);
+const bryantImage = new URL('./images/bryant.jpg', import.meta.url)
+
+const whoIsTheGoat = [
+  // меняем исходные пути на переменные
+  { name: 'Michael Jordan', link: jordanImage },
+  { name: 'Lebron James', link: jamesImage },
+  { name: 'Kobe Bryant', link: bryantImage },
+];
+
+import './styles/index.css'; // добавьте импорт главного файла стилей 
+
